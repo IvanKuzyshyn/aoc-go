54e73c032238eb4d26c101cc1a99ce8dc4fa3edf
Ivan Kuzyshyn <ivan.ku@miro.com>
2024-10-27 14:51:23 +0100

    Add day and year parameters

--- cmd/run.go
+++ cmd/run.go
@@ -2,12 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
 )
 
 type runCommand struct {
+	Day   int8
 	Debug bool
+	Year  int8
 }
 
 func NewRunCommand() *cobra.Command {
@@ -32,4 +35,16 @@ func (c *runCommand) runE(command *cobra.Command, args []string) error {
 
 func (c *runCommand) bindFlags(cmd *cobra.Command) {
 	cmd.Flags().BoolVar(&c.Debug, "debug", false, "Enable debug")
+	cmd.Flags().Int8VarP(&c.Day, "day", "d", 0, "Advent day number")
+	cmd.Flags().Int8VarP(&c.Year, "year", "y", 0, "Advent year")
+
+	err := cmd.MarkFlagRequired("day")
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	err = cmd.MarkFlagRequired("year")
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
 }
