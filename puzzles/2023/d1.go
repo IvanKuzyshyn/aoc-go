126eddb5c56bcde3ef1838c8a37a40e411e18284
Ivan Kuzyshyn <ivan.ku@miro.com>
2024-11-24 16:32:58 +0100

    Resolve race condition

--- puzzles/2023/d1.go
+++ puzzles/2023/d1.go
@@ -1,8 +1,11 @@
 package y2023
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"unicode"
 
 	"github.com/IvanKuzyshyn/aoc-go/solver"
@@ -10,39 +13,72 @@ import (
 
 type Day1Solver struct{}
 
-func (s Day1Solver) Solve(opts solver.Opts) (solver.Result, error) {
-	var totalCalibrationVal int
+type calibrator struct {
+	rows  chan string
+	wg    sync.WaitGroup
+	total int64
+}
 
-	for _, row := range strings.Split(opts.Input, "\n") {
-		if row == "" {
-			continue
-		}
+func (c *calibrator) addRow(row string) {
+	if len(row) == 0 {
+		fmt.Println("Skipped row")
+		// Ignore empty rows
+		return
+	}
+	c.rows <- row
+}
 
-		var firstDigit string
-		var lastDigit string
+func (c *calibrator) startCalculation() {
+	for r := range c.rows {
+		c.wg.Add(1)
+		go c.calcRowCalibrationVal(r)
+	}
+}
 
-		for _, r := range row {
-			if unicode.IsDigit(r) {
-				if firstDigit == "" {
-					firstDigit = string(r)
-					continue
-				}
+func (c *calibrator) calcRowCalibrationVal(row string) {
+	defer c.wg.Done()
+	var firstDigit string
+	var lastDigit string
 
-				lastDigit = string(r)
+	for _, r := range row {
+		if unicode.IsDigit(r) {
+			if firstDigit == "" {
+				firstDigit = string(r)
+				continue
 			}
-		}
 
-		if lastDigit == "" {
-			lastDigit = firstDigit
+			lastDigit = string(r)
 		}
-		totalInRow, err := strconv.Atoi(firstDigit + lastDigit)
-		if err != nil {
-			return solver.Result{}, err
-		}
-		totalCalibrationVal += totalInRow
 	}
 
+	if lastDigit == "" {
+		lastDigit = firstDigit
+	}
+	total, err := strconv.Atoi(firstDigit + lastDigit)
+	if err != nil {
+		// If one row fails the whole result fails
+		panic(err)
+	} else {
+		atomic.AddInt64(&c.total, int64(total))
+	}
+}
+
+func (s Day1Solver) Solve(opts solver.Opts) (solver.Result, error) {
+	c := calibrator{
+		rows:  make(chan string),
+		total: 0,
+	}
+	rows := strings.Split(opts.Input, "\n")
+
+	go c.startCalculation()
+
+	for _, row := range rows {
+		c.addRow(row)
+	}
+	close(c.rows)
+	c.wg.Wait()
+
 	return solver.Result{
-		Output: strconv.Itoa(totalCalibrationVal),
+		Output: strconv.Itoa(int(c.total)),
 	}, nil
 }
