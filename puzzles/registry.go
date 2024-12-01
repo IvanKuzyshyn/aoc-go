33c293497f184e2f39525815034f05989e487996
Ivan Kuzyshyn <ivan.ku@miro.com>
2024-12-01 16:23:08 +0100

    Add solution to the registry

--- puzzles/registry.go
+++ puzzles/registry.go
@@ -2,21 +2,28 @@ package puzzles
 
 import (
 	y2023 "github.com/IvanKuzyshyn/aoc-go/puzzles/2023"
+	y2024 "github.com/IvanKuzyshyn/aoc-go/puzzles/2024"
 	"github.com/IvanKuzyshyn/aoc-go/solver"
 )
 
 type puzzle struct {
-	Day    int16
-	Year   int16
-	Solver solver.Solver
+	Day      int16
+	Year     int16
+	Solution solver.Solver
 }
 
 var (
 	allPuzzles = []puzzle{
 		{
-			Day:    1,
-			Year:   2023,
-			Solver: y2023.Day1Solver{},
+			Day:      1,
+			Year:     2023,
+			Solution: y2023.Day1Solution{},
+		},
+		// Year 2024
+		{
+			Day:      1,
+			Year:     2024,
+			Solution: y2024.Day1Solution{},
 		},
 	}
 )
@@ -24,7 +31,7 @@ var (
 func NewPuzzlesRegistry() *solver.Registry {
 	registry := solver.NewRegistry()
 	for _, pzl := range allPuzzles {
-		registry.Register(pzl.Year, pzl.Day, pzl.Solver)
+		registry.Register(pzl.Year, pzl.Day, pzl.Solution)
 	}
 	return registry
 }
